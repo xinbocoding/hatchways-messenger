5e1b4330fa911a0376c23c54c1b91b40656ed04e
xinbo wang <xinbo.wang.dt@gmail.com>
2021-06-22 20:00:58 -0700

    fix: fix the conversation order

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -20,9 +20,9 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "createdAt", "ASC"]],
+      order: [[Message, "createdAt", "DESC"]],
       include: [
-        { model: Message, order: ["createdAt", "DESC"] },
+        { model: Message },
         {
           model: User,
           as: "user1",
@@ -68,6 +68,7 @@ router.get("/", async (req, res, next) => {
         convoJSON.otherUser.online = false;
       }
 
+      convoJSON.messages.reverse();
       // set properties for notification count and latest message preview
       convoJSON.latestMessageText = convoJSON.messages[convoJSON.messages.length - 1].text;
       conversations[i] = convoJSON;
