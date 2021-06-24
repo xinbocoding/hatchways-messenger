88a92a4d3917c256a3b20c79c1992c7138b5d01d
xinbo wang <xinbo.wang.dt@gmail.com>
2021-06-24 12:07:50 -0700

    change: send 403 instead of 400

--- server/routes/api/messages.js
+++ server/routes/api/messages.js
@@ -11,11 +11,6 @@ router.post("/", async (req, res, next) => {
     const senderId = req.user.id;
     const { recipientId, text, conversationId, sender } = req.body;
 
-    // if we already know conversation id, we can save time and just add it to message and return
-    // if (conversationId) {
-    //   const message = await Message.create({ senderId, text, conversationId });
-    //   return res.json({ message, sender });
-    // }
     // if we don't have conversation id, find a conversation to make sure it doesn't already exist
     let conversation = await Conversation.findConversation(
       senderId,
@@ -37,7 +32,7 @@ router.post("/", async (req, res, next) => {
       return res.json({ message, sender });
       //if conversation exists but request id doesn't match conversation's, return 400 not found
     } else if (conversation && conversation.id !== conversationId) {
-      return res.sendStatus(400);
+      return res.sendStatus(403);
     }
     
     const message = await Message.create({
